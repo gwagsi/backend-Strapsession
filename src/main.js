6341edef63cf2972ab21ff3756bcc662e6359119
dietrichd703@gmail.com <dietrichd703@gmail.com>
2020-11-26 13:49:49 +0100

    huruku

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ const app = new Koa()
 app.use(cors())
 app.use(bodyParser())
 app.use(router.routes()).use(router.allowedMethods())
-
-app.listen(5000, (err) => {
+const port = process.env.PORT || 5050;
+app.listen(port, (err) => {
   if (err) throw err
-  console.log('Server ready at http://localhost:5000')
-  console.log('Process', process.env.NODE_ENV || 'development')
+  console.log('Server ready at http://localhost:5050')
+  console.log('Process', port || 'development')
 })
